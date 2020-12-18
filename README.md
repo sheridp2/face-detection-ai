0ac1c09c666c625fcbf7715001c037a031582fb1
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-18 14:45:35 -0800

    face-api models added, index and script files created and displaying webcam in browser

--- README.md
+++
@@ -1,2 +0,0 @@
-# face-detection-ai
-JS face detection app built with Face API JS library on Tensor Flow
---
+++ script.js
@@ -0,0 +1,20 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  console.log('Camera on');
+})
